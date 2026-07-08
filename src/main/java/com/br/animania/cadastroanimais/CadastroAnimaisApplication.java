5bddb2efced332c858b04802b2a133d1ef627ab8
Axel <axelddlima@hotmail.com>
2026-07-08 18:17:20 -0300

    estrutura inicial do projeto desafio dio.
    Signed-off-by: Axel <axelddlima@hotmail.com>

---
+++ src/main/java/com/br/animania/cadastroanimais/CadastroAnimaisApplication.java
@@ -0,0 +1,27 @@
+package com.br.animania.cadastroanimais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto Spring boot criado para estudo e aplicação de conhecimento.
+ * Depencias ultilizadas:
+ * -Data JPA
+ * -Banco de Dados H2
+ * -Lombok
+ * -Openapi-ui
+ *
+ * @author Axel d. Lima
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+public class CadastroAnimaisApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CadastroAnimaisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/animania/cadastroanimais/CadastroAnimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.animania.cadastroanimais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAnimaisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
